86066edd091ef6ee4a9c520838a861eed2fbfbef
jaderswift <jades@manatts.com>
2019-11-13 09:44:02 -0600

    Added favicon

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,8 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require semantic-ui
+//= require semantic-ui/dropdown
+//= require_tree .
